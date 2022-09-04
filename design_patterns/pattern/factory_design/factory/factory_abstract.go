238d540095879b9315435ff900f078bd27d500a6
chenglong <chenglong@chenglongdeMBP2.lan>
2022-09-04 22:02:19 +0800

    工程类

---
+++ design_patterns/pattern/factory_design/factory/factory_abstract.go
@@ -0,0 +1,27 @@
+package factory
+
+type ISystemConfigParser interface {
+	ParseSystem(data []byte)
+}
+
+type jsonSystemConfigParser struct{}
+
+func (j jsonSystemConfigParser) ParseSystem(data []byte) {
+
+}
+
+// IConfigParserFactory 工厂方法接口
+type IConfigParserFactory interface {
+	CreateRuleParser() IRuleConfigParser
+	CreateSystemParser() ISystemConfigParser
+}
+
+type jsonConfigParserFactory struct{}
+
+func (j jsonConfigParserFactory) CreateRuleParser() IRuleConfigParser {
+	return jsonRuleConfigParser{}
+}
+
+func (j jsonConfigParserFactory) CreateSystemParser() ISystemConfigParser {
+	return jsonSystemConfigParser{}
+}
